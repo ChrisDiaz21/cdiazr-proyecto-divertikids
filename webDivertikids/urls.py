796ee63bb053a52b18da7d29e5041445d678c626
GeraldAlexi <Gerald.morales03@inacapmail.cl>
2024-11-18 17:04:00 -0300

    recuperar contraseña
    aun no se puede hacer ya que no se envían los correo y no se como hacerlo dsajdsjajd

--- webDivertikids/urls.py
+++ webDivertikids/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home,name='home'),
@@ -8,4 +9,8 @@ urlpatterns = [
     path('log_in/', views.log_in, name='log_in'),
     path('register/', views.register, name='register'),
     path('productos/', views.productos, name='productos'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="core/password_reset.html"), name='password_reset'),
+    path('reset_password_send/', auth_views.PasswordResetDoneView.as_view(template_name="core/password_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="core/password_complete.html"), name='password_reset_complete'),
 ]
