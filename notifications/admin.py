7e546cc1458eb3131a778ea9d2383a0aebf89d25
ChrisDiaz21 <137074898+ChrisDiaz21@users.noreply.github.com>
2024-12-15 22:41:40 -0300

    Ajustes fecha y hora

--- notifications/admin.py
+++ notifications/admin.py
@@ -6,4 +6,8 @@ class NotificationAdmin(admin.ModelAdmin):
     list_display = ('message', 'created_at')
     ordering = ('-created_at',)
 
-
+def get_list_display(self, request): 
+    return ('Mensaje', 'Fecha') 
+    
+def get_ordering(self, request): 
+    return ('-Fecha',)
--- notifications/models.py
+++ notifications/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Notification(models.Model):
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(verbose_name="Mensaje")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha")
 
     def __str__(self):
         return f"{self.message} - {self.created_at}"
